f7a233c773fe23760f546142cc41ad9d4e471d09
Anthony Schreiber <schreiberanthony72@gmail.com>
2020-12-14 23:15:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 botCfg = {
     'USERNAME' : 'default',
-    'desired_cards': ["3070, 3080, 3090, 3060ti"],
+    'desired_cards': ["3070", "3080", "3090", "3060ti"],
     'PASSWORD' : 'default',
     'auto_cart': True,
 }
